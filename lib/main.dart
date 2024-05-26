4738a7ffdfbdefe04f73f749b2dd6bf89be99371
I10n <rioriop222@gmail.com>
2024-05-26 12:25:26 +0900

    CREATE Timer_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:noodle_timer_f/timer_display.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const CountDownPage(duration: 10, onReset: call,),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tick Tack Tick Tack...'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("STOP"),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => NextPage()));
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class NextPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Timer Setting '),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("START"),
-          onPressed: () {
-            // ここにボタンを押した時に呼ばれるコードを書く
-            Navigator.pop(context);
-          },
-        ),
-      ),
-    );
-  }
-}
+void call() => print('RESET TIMER');
