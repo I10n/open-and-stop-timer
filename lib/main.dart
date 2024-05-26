798d117436a83e5d16071f90f2d9ecf0be431a63
I10n <rioriop222@gmail.com>
2024-05-26 14:04:57 +0900

    CHANGE Navigation: NextPage to TimerScreen with TimerScreen initializing

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,57 @@ import 'package:noodle_timer_f/timer_display.dart';
 void main() {
   runApp(const MyApp());
 }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.green,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatelessWidget {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return CountDownPage(duration: 10,
+//       onReset: () {
+//         Navigator.push(
+//             context, MaterialPageRoute(builder: (context) => NextPage()));
+//       },
+//     );
+//   }
+// }
+//
+// class NextPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Timer Setting '),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           child: Text("START"),
+//           onPressed: () {
+//             // ここにボタンを押した時に呼ばれるコードを書く
+//             Navigator.pop(context);
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -16,20 +67,23 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyHomePage(),
+        '/timer_setting': (context) => NextPage()
+      },
+
     );
   }
 }
+
 
 class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return CountDownPage(duration: 10,
-      onReset: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => NextPage()));
-      },
+      onReset: () => Navigator.of(context).pushNamed('/timer_setting')
     );
   }
 }
@@ -44,10 +98,7 @@ class NextPage extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           child: Text("START"),
-          onPressed: () {
-            // ここにボタンを押した時に呼ばれるコードを書く
-            Navigator.pop(context);
-          },
+          onPressed: () => Navigator.of(context).pushNamed('/')
         ),
       ),
     );
